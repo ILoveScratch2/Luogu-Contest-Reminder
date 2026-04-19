be3ca0b851c60f4bc16bb085cc168501177556eb
ILoveScratch2 <ilovescratch@foxmail.com>
2026-04-19 17:09:42 +0800

    fix: uvicorn ASGI 错误

--- backend/main.py
+++ backend/main.py
@@ -1004,4 +1004,4 @@ if __name__ == "__main__":
     finally:
         db.close()
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=False)
